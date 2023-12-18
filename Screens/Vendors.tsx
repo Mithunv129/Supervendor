07f7f97c35a2f2d1a9a5851fe7b09546237ec30c
LogarajP <logupalani2003@gmail.com>
2023-12-18 12:57:46 +0530

    made vendors responsive

--- Screens/Vendors.tsx
+++ Screens/Vendors.tsx
@@ -1,9 +1,74 @@
-import { StyleSheet,Text,View,Image} from 'react-native';
+// import { StyleSheet,Text,View,Image} from 'react-native';
+// import VendorsBody from '../components/VendorsBody';
+// function Vendors(){
+//     return(
+//         <View>
+//             <VendorsBody/>
+//         </View>
+//     )
+// }
+
+// export default Vendors;
+
+// import {Text, View, StyleSheet,Dimensions} from 'react-native';
+// import SelectLocationHeader from '../components/SelectLocationHeader';
+// import SelectLocationBody from '../components/SelectLocationBody';
+// import {
+//   widthPercentageToDP as wp,
+//   heightPercentageToDP as hp,
+// } from 'react-native-responsive-screen';
+// import { ScrollView } from 'react-native';
+// const {width, height, scale} = Dimensions.get('window');
+
+// function SelectLocation() {
+//   return (
+//     <View style={{flex: 1, backgroundColor: '#F3F5F9'}}>
+//       <ScrollView style={{marginRight:wp((24/width)*100),marginLeft:wp((24/width)*100),}}>
+//         <View style={styles.header}>
+//           <SelectLocationHeader />
+//         </View>
+//         <View style={styles.body}>
+//           <SelectLocationBody />
+//         </View>
+//       </ScrollView >
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//  header: {
+//    flex: 1,
+//   //   padding: 24,
+//   },
+//   body: {
+//    // flex: 3.5,
+//     //marginBottom:'100%',
+//   //  backgroundColor:'red',
+    
+//   },
+// });
+
+// export default SelectLocation;
+
+
+
+
+import { StyleSheet,Text,View,Image,Dimensions} from 'react-native';
 import VendorsBody from '../components/VendorsBody';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const {width, height, scale} = Dimensions.get('window');
+
 function Vendors(){
     return(
-        <View>
-            <VendorsBody/>
+      <View style={{flex:1,backgroundColor:"#F3F5F9"}}>
+        <View style={{marginLeft:wp((24/width)*100),marginRight:wp((24/width)*100)}}>
+          
+          <VendorsBody/>
+
+          
+        </View>
         </View>
     )
 }
--- components/VendorsBody.tsx
+++ components/VendorsBody.tsx
@@ -1,5 +1,175 @@
+// import React, {useState} from 'react';
+// import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+// import SvgImageAssignTeam from '../assets/svgTeamAssign';
+// import SvgImageNext from '../assets/svgNext';
+// import SvgImageLock from '../assets/lock';
+// import SvgImageNewJob from '../assets/svgCard';
+// import SvgNote from '../assets/SvgNote';
+// import SvgNote2 from '../assets/SvgNote';
+// import {useNavigation} from '@react-navigation/native';
+// import {Avatar, Button, Card, Title} from 'react-native-paper';
+// export function Vendors_Body(): JSX.Element {
+//   const navigation: any = useNavigation();
+//   const NavigateToNextPage = () => {
+//     navigation.navigate('SubContact_Summary');
+//   };
+//   const [actions, setAction] = useState([
+//     {text: 'All available vendors', key: '1', borderColor: '#F3F5F9'},
+//     {text: 'My preferred vendors', key: '2', borderColor: '#F3F5F9'},
+//     {text: 'Search for vendors', key: '3', borderColor: '#F3F5F9'},
+//     {text: 'Invite vendors I know to ARK', key: '4', borderColor: '#F3F5F9'},
+//   ]);
+//   const handlePress = (key: string) => {
+//     const updatedActions = [...actions];
+
+//     // Find the selected item and update its borderColor
+//     const selectedItem = updatedActions.find(item => item.key === key);
+//     if (selectedItem) {
+//       selectedItem.borderColor =
+//         selectedItem.borderColor === '#F3F5F9' ? '#3366FF' : '#F3F5F9';
+//     }
+
+//     // Update the state with the modified actions array
+//     setAction(updatedActions);
+//   };
+//   return (
+//     <View style={styles.container1}>
+//       {actions.map(item => {
+
+//         return (
+//           <TouchableOpacity
+//             onPress={() => handlePress(item.key)}
+//             key={item.key}>
+//             <View
+//               key={item.key}
+//               style={[styles.Box, {borderColor: item.borderColor}]}>
+//               <View style={styles.container}>
+//                 <View style={styles.Icon}>
+//                   <Text style={styles.Text}>{item.text}</Text>
+//                 </View>
+//               </View>
+//             </View>
+//           </TouchableOpacity>
+//         );
+//       })}
+//        <View style={styles.ButtonView}>
+//         <View style={styles.Button1}>
+//           <Button onPress={() => {}} labelStyle={styles.saveAndExit}>
+//             save & Exit
+//           </Button>
+//         </View>
+//         <View style={styles.Button2}>
+//           <Button onPress={NavigateToNextPage} labelStyle={styles.openForBids}>
+//             continue
+//           </Button>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container1: {
+//     backgroundColor: '#F3F5F9',
+//     height: '100%',
+//   },
+//   container: {
+//     flexDirection: 'row',
+//   },
+//   Box: {
+//     margin: '4%',
+//     paddingTop: '6%',
+//     paddingBottom: '6%',
+//     marginTop: '5%',
+//     borderRadius: 12,
+//     borderWidth: 1,
+//     backgroundColor: '#FFF',
+//     shadowColor: 'rgba(51, 102, 255, 0.12)',
+//     shadowOffset: {width: 0, height: 4},
+//     shadowOpacity: 1,
+//     shadowRadius: 8,
+//     elevation: 2,
+//     paddingLeft: '8%',
+//   },
+//   Text: {
+//     color: '#101840',
+//     fontFamily: 'Manrope',
+//     fontSize: 18,
+//     fontStyle: 'normal',
+//     fontWeight: '400',
+//     lineHeight: 20,
+//   },
+
+//   buttonLabel: {
+//     color: 'white',
+//     fontFamily: 'Lato',
+//     fontSize: 19,
+//     fontStyle: 'normal',
+//     fontWeight: '500',
+//     lineHeight: 24,
+//     letterSpacing: 0.15,
+//   },
+//   ButtonView: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-around',
+//    paddingBottom: '-6%',
+//     flex: 3,
+//     display: 'flex',
+//     alignItems: 'center',
+
+//   },
+//   saveAndExit: {
+//     color: '#36F',
+
+//     fontFamily: 'Lato',
+//     fontSize: 16,
+//     fontWeight: '500',
+//     lineHeight: 24,
+//     letterSpacing: 0.15,
+//   },
+//   openForBids: {
+//     color: 'white',
+//     fontFamily: 'Lato',
+//     fontSize: 19,
+//     fontStyle: 'normal',
+//     fontWeight: '500',
+//     lineHeight: 24,
+//     letterSpacing: 0.15,
+//   },
+//   Button1: {
+//     width: 160,
+//     height: 60,
+//     borderColor: '#36F',
+//     borderWidth: 1,
+//     marginLeft: '4%',
+//     marginRight: '4%',
+//     backgroundColor: '#FFF',
+//     borderRadius: 12,
+//     marginTop: '52%',
+//     paddingTop: '2%',
+//     paddingBottom: '2%',
+//   },
+//   Button2: {
+//     width: 160,
+//     height: 60,
+//     marginLeft: '4%',
+//     marginRight: '4%',
+//     backgroundColor: '#36F',
+//     borderRadius: 12,
+//     marginTop: '52%',
+//     paddingTop: '2%',
+//     paddingBottom: '2%',
+//   },
+// });
+// export default Vendors_Body;
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import SvgImageAssignTeam from '../assets/svgTeamAssign';
 import SvgImageNext from '../assets/svgNext';
 import SvgImageLock from '../assets/lock';
@@ -8,6 +178,12 @@ import SvgNote from '../assets/SvgNote';
 import SvgNote2 from '../assets/SvgNote';
 import {useNavigation} from '@react-navigation/native';
 import {Avatar, Button, Card, Title} from 'react-native-paper';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const {width, height, scale} = Dimensions.get('window');
+
 export function Vendors_Body(): JSX.Element {
   const navigation: any = useNavigation();
   const NavigateToNextPage = () => {
@@ -34,8 +210,21 @@ export function Vendors_Body(): JSX.Element {
   };
   return (
     <View style={styles.container1}>
+      <View style={{marginTop: hp((20 / height) * 100)}}>
+        <Text
+          style={{
+            color: '#101840',
+            fontFamily: 'Manrope',
+            fontSize: hp(2.1),
+            fontStyle: 'normal',
+            fontWeight: '400',
+            lineHeight: 22,
+          }}>
+          How would you like to select vendors to {'\n'}bid for the job ?{' '}
+        </Text>
+      </View>
+
       {actions.map(item => {
-       
         return (
           <TouchableOpacity
             onPress={() => handlePress(item.key)}
@@ -43,25 +232,32 @@ export function Vendors_Body(): JSX.Element {
             <View
               key={item.key}
               style={[styles.Box, {borderColor: item.borderColor}]}>
-              <View style={styles.container}>
-                <View style={styles.Icon}>
-                  <Text style={styles.Text}>{item.text}</Text>
-                </View>
-              </View>
+              <Text style={styles.Text}>{item.text}</Text>
             </View>
           </TouchableOpacity>
         );
       })}
-       <View style={styles.ButtonView}>
-        <View style={styles.Button1}>
-          <Button onPress={() => {}} labelStyle={styles.saveAndExit}>
-            save & Exit
-          </Button>
-        </View>
-        <View style={styles.Button2}>
-          <Button onPress={NavigateToNextPage} labelStyle={styles.openForBids}>
-            continue
-          </Button>
+      <View
+        style={{
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'flex-end',
+          marginBottom: hp((100 / height) * 100),
+        }}>
+        <View style={styles.ButtonView}>
+          <View style={styles.Button1}>
+            <Button onPress={() => {}} labelStyle={styles.saveAndExit}>
+              save & Exit
+            </Button>
+          </View>
+          <View style={styles.Button2}>
+            <Button
+              onPress={NavigateToNextPage}
+              labelStyle={styles.openForBids}>
+              {/* <Button  labelStyle={styles.openForBids}> */}
+              continue
+            </Button>
+          </View>
         </View>
       </View>
     </View>
@@ -71,16 +267,17 @@ export function Vendors_Body(): JSX.Element {
 const styles = StyleSheet.create({
   container1: {
     backgroundColor: '#F3F5F9',
-    height: '100%',
+    height: hp(100),
+    // height: '100%',
   },
-  container: {
-    flexDirection: 'row',
-  },
+  // container: {
+  //   flexDirection: 'row',
+  // },
   Box: {
-    margin: '4%',
-    paddingTop: '6%',
-    paddingBottom: '6%',
-    marginTop: '5%',
+    // margin: '4%',
+    paddingTop: hp((18 / height) * 100),
+    paddingBottom: hp((20 / height) * 100),
+    marginTop: hp((16 / height) * 100),
     borderRadius: 12,
     borderWidth: 1,
     backgroundColor: '#FFF',
@@ -89,40 +286,31 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 8,
     elevation: 2,
-    paddingLeft: '8%',
+    paddingLeft: wp((20 / width) * 100),
   },
   Text: {
     color: '#101840',
     fontFamily: 'Manrope',
-    fontSize: 18,
+    fontSize: hp(2),
     fontStyle: 'normal',
     fontWeight: '400',
     lineHeight: 20,
   },
- 
-  buttonLabel: {
-    color: 'white',
-    fontFamily: 'Lato',
-    fontSize: 19,
-    fontStyle: 'normal',
-    fontWeight: '500',
-    lineHeight: 24,
-    letterSpacing: 0.15,
-  },
+
   ButtonView: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-   paddingBottom: '-6%',
-    flex: 3,
-    display: 'flex',
-    alignItems: 'center',
-    
+
+    // paddingBottom: '-6%',
+    // flex: 3,
+    //display: 'flex',
+    // alignItems: 'center',
   },
   saveAndExit: {
     color: '#36F',
 
     fontFamily: 'Lato',
-    fontSize: 16,
+    fontSize: hp(1.9),
     fontWeight: '500',
     lineHeight: 24,
     letterSpacing: 0.15,
@@ -130,35 +318,35 @@ const styles = StyleSheet.create({
   openForBids: {
     color: 'white',
     fontFamily: 'Lato',
-    fontSize: 19,
+    fontSize: hp(1.9),
     fontStyle: 'normal',
     fontWeight: '500',
     lineHeight: 24,
     letterSpacing: 0.15,
   },
   Button1: {
-    width: 160,
-    height: 60,
+    width: wp(40),
+    height: hp(7),
     borderColor: '#36F',
     borderWidth: 1,
-    marginLeft: '4%',
-    marginRight: '4%',
+    // marginLeft: '4%',
+    // marginRight: '4%',
     backgroundColor: '#FFF',
     borderRadius: 12,
-    marginTop: '52%',
-    paddingTop: '2%',
-    paddingBottom: '2%',
+    // marginTop: '52%',
+    paddingTop: hp((6 / height) * 100),
+    paddingBottom: hp((6 / height) * 100),
   },
   Button2: {
-    width: 160,
-    height: 60,
-    marginLeft: '4%',
-    marginRight: '4%',
+    width: wp(40),
+    height: hp(7),
+    // marginLeft: '4%',
+    // marginRight: '4%',
     backgroundColor: '#36F',
     borderRadius: 12,
-    marginTop: '52%',
-    paddingTop: '2%',
-    paddingBottom: '2%',
+    // marginTop: '52%',
+    paddingTop: hp((6 / height) * 100),
+    paddingBottom: hp((6 / height) * 100),
   },
 });
 export default Vendors_Body;
